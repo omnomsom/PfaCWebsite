5ce706f2dfaf504692b4c2d4241fb9e4187fe10e
Soumya <arsoumya21@gmail.com>
2023-09-15 05:22:49 +0530

    Initial RestAPIs for Animals

--- backend/controllers/animalController.js
+++ backend/controllers/animalController.js
@@ -1,27 +1,48 @@
 const asyncHandler = require("express-async-handler");
+const Animal = require("../models/animalModel");
 
 const getAnimals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Got animals!",
-  });
+  const animals = await Animal.find();
+  res.status(200).json(animals);
 });
 
 const setAnimal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Set animals!",
-  });
+  try {
+    console.log(req.body);
+    const animal = await Animal.create({
+      name: req.body.name,
+    });
+    res.status(200).json(animal);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the animal." });
+  }
 });
 
 const updateAnimal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Updated animal ${req.params.id}!`,
-  });
+  const animal = await Animal.findById(req.params.id);
+
+  if (!animal) {
+    res.status(400).json({ error: "Animal not found" });
+  }
+
+  const updatedAnimal = await Animal.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedAnimal);
 });
 
 const deleteAnimal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Deleted animal ${req.params.id}!`,
-  });
+  const animal = await Animal.findById(req.params.id);
+
+  if (!animal) {
+    res.status(400).json({ error: "Animal not found" });
+  }
+  await animal.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/models/animalModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const animalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Animal = mongoose.model("Animal", animalSchema);
+
+module.exports = Animal;
